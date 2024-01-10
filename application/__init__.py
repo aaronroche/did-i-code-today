20e22c9b8b951ef0ccc441ddb5b4ad679a5746da
aaronroche <aaron.roche4567@gmail.com>
2024-01-10 09:45:09 -0500

    Remove secret

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "e943460b5c5d15b27a0755a6aa7326747ca69258"
-app.config["MONGO_URI"] = "mongodb+srv://aaronroche:8dKk6LiqaUW7SDgY@cluster0.dixnrws.mongodb.net/?retryWrites=true&w=majority"
+app.config["SECRET_KEY"] = "blah"
+app.config["MONGO_URI"] = "blah"
 
 # Create a new client and connect to the server
 mongodb_client = PyMongo(app)
